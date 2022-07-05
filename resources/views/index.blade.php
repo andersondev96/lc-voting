4b5487af2d1daaf33172a18fa60c0130de6d18c3
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-07-05 17:31:41 -0300

    Initial setup and design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Category One">Category One</option>
+                <option value="Category One">Category Two</option>
+                <option value="Category One">Category Three</option>
+                <option value="Category One">Category Four</option>
+            </select>
+        </div>
+
+        <div class="w-1/3">
+            <select name="other filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Filter One">Filter One</option>
+                <option value="Filter One">Filter Two</option>
+                <option value="Filter One">Filter Three</option>
+                <option value="Filter One">Filter Four</option>
+            </select>
+        </div>
+
+        <div class="w-2/3 relative">
+            <input type="search" placeholder="Find an idea"
+                class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
